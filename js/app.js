7c80f7f88f50840e7ac2b5a62eaedf0161b798d9
Proshenjit Karmakar <proshnjit@gmail.com>
2022-06-15 12:59:02 +0600

    group project

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("RangeValue");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function(){
+    output.innerHTML = (this.value);
+}
+
+slider.addEventListener("mousemove", function(){
+    var x = slider.value;
+    var color = 'linear-gradient(90deg, rgb(192,162,83)' + x+'%,rgb(214,214,214)'+x+'%)';
+    slider.style.background = color;
+})
+
+//Section One SLider JS
+function animeSlide(){ 
+  document.getElementById("content2").style.zIndex = "1";
+
+  $("#content1").animate({
+    "left":"-105px"
+  },"slow");
+ }
